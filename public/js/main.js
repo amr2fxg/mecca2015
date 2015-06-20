822bd379c7cd914c35b77dd7b2ed5da4b806457b
amr2fxg <dinhog@hotmail.com>
2015-06-20 12:57:12 -0300

    main.js

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+
+<script type="text/javascript">
+/*  
+* Habilitar essa opção e iniciar controle de versão dos aquivos .js .css quando entrar em produção (caching p/ performance!) 
+*
+$.ajaxSetup({
+cache: true
+});  
+*
+*/
+
+	//document ready only scripts
+	$(document).ready(function() {
+
+	$.getScript("/js/utils/search.js");
+
+	}); //document.ready
+</script>
